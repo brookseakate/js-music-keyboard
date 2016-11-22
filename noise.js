3b9d9d5636f9dd821aa9ec082c7bfef55e8f3c4f
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-21 16:51:23 -0800

    Working solution; needs cleanup

--- noise.js
+++ noise.js
@@ -1,3 +1,23 @@
+var playAudio = function(letter) {
+  console.log("play " + letter);
+  var audioId = $("#" + letter + "Audio")[0];
+  audioId.load();
+  audioId.play();
+};
+
 $(document).ready( function() {
-  // your code here
+  $('.note').on('click', function(event) {
+    var classes = $(this).attr('class');
+    // var classArr = Array.from(classes);
+    console.log("Classes: " + classes);
+    // console.log("classArr: " + classArr);
+    console.log("Classes[5]: " + classes[5]); // @TODO: make this better
+    var letter = classes[5];
+    playAudio(letter);
+  });
+
+  $('body').keydown(function(event) {
+    console.log("Key: "+ event.key);
+    playAudio(event.key);
+  });
 });
